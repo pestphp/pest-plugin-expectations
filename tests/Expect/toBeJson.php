2d953afb59bdd162b5f87452af583e93ad276e50
Nuno Maduro <enunomaduro@gmail.com>
2020-12-26 21:25:25 +0100

    tests: refactor json tests

--- tests/Expect/toBeJson.php
+++ tests/Expect/toBeJson.php
@@ -2,24 +2,16 @@
 
 use PHPUnit\Framework\ExpectationFailedException;
 
-beforeEach(function () {
-    $this->user = [
-        'id'    => 1,
-        'name'  => 'Nuno',
-        'email' => 'enunomaduro@gmail.com',
-    ];
-});
-
 test('pass', function () {
-    expect(json_encode($this->user))->toBeJson();
-    expect($this->user)->not->toBeJson();
-    expect($this->user['name'])->not->toBeJson();
+    expect('{"hello":"world"}')->toBeJson();
+    expect('foo')->not->toBeJson();
+    expect('{"hello"')->not->toBeJson();
 });
 
 test('failures', function () {
-    expect($this->user['name'])->toBeJson();
+    expect(':"world"}')->toBeJson();
 })->throws(ExpectationFailedException::class);
 
 test('not failures', function () {
-    expect(json_encode($this->user))->not->toBeJson();
+    expect('{"hello":"world"}')->not->toBeJson();
 })->throws(ExpectationFailedException::class);
