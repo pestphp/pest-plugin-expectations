2af3a4f65a6448a0031183fd36cf7647423f6418
Fabio Ivona <fabio.ivona@defstudio.it>
2021-06-13 23:27:41 +0200

    adds tests for dot notation in ->toHaveKey expectation

--- tests/Expect/toHaveKey.php
+++ tests/Expect/toHaveKey.php
@@ -2,14 +2,91 @@
 
 use PHPUnit\Framework\ExpectationFailedException;
 
-test('pass', function () {
-    expect(['a' => 1, 'b', 'c' => 'world'])->toHaveKey('c');
-});
+dataset('test array', [
+    'test array' => [
+        'array' => [
+            'a'             => 1,
+            'b',
+            'c'             => 'world',
+            'd'             => [
+                'e' => 'hello',
+            ],
+            'key.with.dots' => false,
+        ],
+    ],
+]);
 
-test('failures', function () {
-    expect(['a' => 1, 'b', 'c' => 'world'])->toHaveKey('hello');
-})->throws(ExpectationFailedException::class);
+test('pass', function ($array, $key) {
+    expect($array)->toHaveKey($key);
+})->with('test array')->with([
+    'plain key'           => 'c',
+    'nested key'          => 'd.e',
+    'plain key with dots' => 'key.with.dots',
+]);
 
-test('not failures', function () {
-    expect(['a' => 1, 'hello' => 'world', 'c'])->not->toHaveKey('hello');
-})->throws(ExpectationFailedException::class);
+test('pass with value', function ($array, $key, $value) {
+    expect($array)->toHaveKey($key, $value);
+})->with('test array')->with([
+    'plain key'           => [
+        'key'   => 'c',
+        'value' => 'world',
+    ],
+    'nested key'          => [
+        'key'   => 'd.e',
+        'value' => 'hello',
+    ],
+    'plain key with dots' => [
+        'key'   => 'key.with.dots',
+        'value' => false,
+    ],
+]);
+
+test('failures', function ($array, $key) {
+    expect($array)->toHaveKey($key);
+})->with('test array')->with([
+    'plain key'           => 'foo',
+    'nested key'          => 'd.bar',
+    'plain key with dots' => 'missing.key.with.dots',
+])->throws(ExpectationFailedException::class);
+
+test('fails with wrong value', function ($array, $key, $value) {
+    expect($array)->toHaveKey($key, $value);
+})->with('test array')->with([
+    'plain key'           => [
+        'key'   => 'c',
+        'value' => 'bar',
+    ],
+    'nested key'          => [
+        'key'   => 'd.e',
+        'value' => 'foo',
+    ],
+    'plain key with dots' => [
+        'key'   => 'key.with.dots',
+        'value' => true,
+    ],
+])->throws(ExpectationFailedException::class);
+
+test('not failures', function ($array, $key) {
+    expect($array)->not->toHaveKey($key);
+})->with('test array')->with([
+    'plain key'           => 'c',
+    'nested key'          => 'd.e',
+    'plain key with dots' => 'key.with.dots',
+])->throws(ExpectationFailedException::class);
+
+test('not failures with correct value', function ($array, $key, $value) {
+    expect($array)->not->toHaveKey($key, $value);
+})->with('test array')->with([
+    'plain key'           => [
+        'key'   => 'c',
+        'value' => 'world',
+    ],
+    'nested key'          => [
+        'key'   => 'd.e',
+        'value' => 'hello',
+    ],
+    'plain key with dots' => [
+        'key'   => 'key.with.dots',
+        'value' => false,
+    ],
+])->throws(ExpectationFailedException::class);
