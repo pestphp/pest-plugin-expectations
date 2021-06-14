f6ef6ed2163387690f7d6d29f5a7761690984583
luke <lukeraymonddowning@gmail.com>
2021-06-14 17:05:54 +0100

    Adds same functionality in `each` call

--- src/Expectation.php
+++ src/Expectation.php
@@ -101,11 +101,20 @@ final class Expectation
 
     /**
      * Creates an expectation on each item of the iterable "value".
+     *
+     * @param callable|mixed $callback
+     * @param mixed          $arguments
+     *
+     * @return Each|Expectation
      */
-    public function each(callable $callback = null): Each
+    public function each($callback = null, ...$arguments)
     {
         if (!is_iterable($this->value)) {
             throw new BadMethodCallException('Expectation value is not iterable.');
+        }
+
+        if (count($arguments) > 0) {
+            return $this->sequence($callback, ...$arguments);
         }
 
         if (is_callable($callback)) {
